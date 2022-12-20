2c5855b094170bd1f2b8eaf4fd6fc19e74eb3a3a
leeeeeehyob <lhb0803@snu.ac.kr>
2022-12-20 22:21:40 +0900

    debug: run latest image

--- operator-practice/operators/ecr_run.sh
+++ operator-practice/operators/ecr_run.sh
@@ -3,7 +3,7 @@ aws ecr get-login-password --region ap-northeast-2 | sudo docker login --usernam
 
 # docker 실행
 sudo docker pull 433166909747.dkr.ecr.ap-northeast-2.amazonaws.com/feature_practice_hb:latest
-sudo docker run --name feature_test 23ad781678f4 python runner.py --m "$1" --t "$2" --c $3
+sudo docker run --name feature_test 433166909747.dkr.ecr.ap-northeast-2.amazonaws.com/feature_practice_hb python runner.py --m "$1" --t "$2" --c $3
 
 # docker container 삭제
 sudo docker rm feature_test
