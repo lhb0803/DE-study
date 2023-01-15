0ada0b691de33e1d9f32e729d8882e8e787734b3
Hyobae Lee <lhb0803@snu.ac.kr>
2023-01-15 15:33:41 +0900

    cronjob from train to docker push

---
+++ operator-practice/operators/update_pycaret_ecr.sh
@@ -0,0 +1,12 @@
+# load data -> train pycaret model
+source ~/anaconda3/etc/profile.d/conda.sh
+conda activate py38
+python train.py
+
+echo "Build Docker Image..."
+sudo docker image build . -t pycaret_lgbm:latest
+
+# push docker image
+sudo docker tag pycaret_lgbm:latest 433166909747.dkr.ecr.ap-northeast-2.amazonaws.com/pycaret_lgbm
+aws ecr get-login-password --region ap-northeast-2 | sudo docker login --username AWS --password-stdin 433166909747.dkr.ecr.ap-northeast-2.amazonaws.com
+sudo docker push 433166909747.dkr.ecr.ap-northeast-2.amazonaws.com/pycaret_lgbm
