8575f6311f49cbec9394b012689fee6d9bbc3416
Hyobae Lee <lhb0803@snu.ac.kr>
2023-01-15 13:46:53 +0900

    build docker image with pycaret

---
+++ modeling/lgbm_api.py
@@ -0,0 +1,22 @@
+
+import pandas as pd
+from pycaret.classification import load_model, predict_model
+from fastapi import FastAPI
+import uvicorn
+
+# Create the app
+app = FastAPI()
+
+# Load trained Pipeline
+model = load_model('lgbm_api')
+
+# Define predict function
+@app.post('/predict')
+def predict(candle_date_time_kst, open, high, low, close, volume):
+    data = pd.DataFrame([[candle_date_time_kst, open, high, low, close, volume]])
+    data.columns = ['candle_date_time_kst', 'open', 'high', 'low', 'close', 'volume']
+    predictions = predict_model(model, data=data) 
+    return {'prediction': list(predictions['Label'])}
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='127.0.0.1', port=8000)
