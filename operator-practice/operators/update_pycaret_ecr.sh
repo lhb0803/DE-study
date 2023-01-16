262a094bd6cea2a3998a521d27244056577b9a68
leeeeeehyob <lhb0803@snu.ac.kr>
2023-01-17 00:29:18 +0900

    rewrite shell script to fix directory prob

--- operator-practice/operators/update_pycaret_ecr.sh
+++ operator-practice/operators/update_pycaret_ecr.sh
@@ -1,7 +1,8 @@
 # load data -> train pycaret model
 source ~/anaconda3/etc/profile.d/conda.sh
 conda activate py38
-python /home/ubuntu/airflow/operators/train.py
+cd /home/ubuntu/airflow/operators/
+python train.py
 
 echo "Build Docker Image..."
 sudo docker image build . -t pycaret_lgbm:latest
@@ -10,3 +11,6 @@ sudo docker image build . -t pycaret_lgbm:latest
 sudo docker tag pycaret_lgbm:latest 433166909747.dkr.ecr.ap-northeast-2.amazonaws.com/pycaret_lgbm
 aws ecr get-login-password --region ap-northeast-2 | sudo docker login --username AWS --password-stdin 433166909747.dkr.ecr.ap-northeast-2.amazonaws.com
 sudo docker push 433166909747.dkr.ecr.ap-northeast-2.amazonaws.com/pycaret_lgbm
+
+# move to home dir
+cd ~
